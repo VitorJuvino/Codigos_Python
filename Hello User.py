247bcfef6c9b9890502e222385f19e1ac73a7bf2
Vitor Juvino <99814359+VitorJuvino@users.noreply.github.com>
2022-12-05 12:16:02 -0300

    Delete Hello User.py

--- Hello User.py
+++
@@ -1,8 +0,0 @@
-# Questao Hello User
-# 22-02-22, Vitor
-
-# Pedir o nome do usuário
-nome = str(input('Qual o seu nome? '))
-# imprima o nome do usuário na saída
-print(f'Olá, {nome}! '
-f'Como voce está, {nome}?')
